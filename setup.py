3f5f7b81c180b175044f1c1599063d156a943b3c
inseeee <kalka.vika.perm@gmail.com>
2026-03-26 22:34:08 +0300

    fix: ensure autochecker finds bot everywhere

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="lms-bot",
-    version="0.1",
-    py_modules=["bot"],
+    version="0.1.0",
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'lms-bot = bot:main',
